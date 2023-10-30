5ffecc7b9773512116309a6872a4e03b35321369
teclu <27724529+teclu@users.noreply.github.com>
2023-10-30 16:41:38 +0800

    chore: deploy

---
+++ redirect.09491742.js
@@ -0,0 +1,3 @@
+const a=window.location.hash.match(/^#access_token=([^&]+).*$/),o=a?.[1];o&&window.location.replace(`baseg-chat://authentication/${o}`),window.location.replace(location.pathname.split("/").slice(0,2).join("/"));//# sourceMappingURL=redirect.09491742.js.map
+
+//# sourceMappingURL=redirect.09491742.js.map
